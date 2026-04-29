df95405f4ce1f1f16c2201298c17e6c7af5628a3
Maya Nikita Baumann <maya.baumann@bluewin.ch>
2026-04-29 15:00:31 +0200

    add register page

---
+++ js/register.js
@@ -0,0 +1,23 @@
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault(); // Prevent form from submitting normally
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    console.log(email, password);
+    
+    try {
+        const response = await fetch('api/register.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
